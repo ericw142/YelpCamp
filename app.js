c0beef2eb983d675e4061eed5723e99f7a896428
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-03-11 12:25:01 -0500

    create functionality

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
 
+// Routing
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -31,11 +33,23 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
 })
 
+
+// Start Server
 app.listen(3000, () => {
     console.log("Serving on port 3000")
 })
