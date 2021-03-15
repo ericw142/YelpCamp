4256878a6f28543bc6e28f7843300cf32d68a63b
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-03-15 13:42:37 -0400

    error page template

--- app.js
+++ app.js
@@ -78,8 +78,9 @@ app.all('*', (req, res, next) => {
 
 // Error Handler
 app.use((err, req, res, next) => {
-    const {statusCode = 500, message = 'Something went wrong!'} = err;
-    res.status(statusCode).send(message);
+    const {statusCode = 500} = err;
+    if (!err.message) err.message = "Something went wrong!";
+    res.status(statusCode).render('error', { err });
 })
 
 // Start Server
