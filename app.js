8be5d58a06ac91d929ff9e548b67bba37c772dca
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-03-15 13:33:55 -0400

    404 error handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Campground = require("./models/campground");
@@ -43,6 +44,7 @@ app.get('/campgrounds/new', (req, res) => {
 })
 // Creating new campground
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError('Invalid campground data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -69,10 +71,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }))
+// 404
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404));
+})
 
 // Error Handler
 app.use((err, req, res, next) => {
-    res.send('Something went wrong!');
+    const {statusCode = 500, message = 'Something went wrong!'} = err;
+    res.status(statusCode).send(message);
 })
 
 // Start Server
