11803c9108f5cdbe1ee29060a07a94b49c882178
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-03-16 13:00:35 -0400

    initial display of reviews

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 }))
 // Finding specific campground
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground });
 }))
 // Edit campground page
