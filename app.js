f3833813095b7696e99be774bb4127cbdc7e30f5
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-03-17 12:52:36 -0400

    initial review delete functionality

--- app.js
+++ app.js
@@ -101,6 +101,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) 
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
 }))
+// Delete review
+app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`)
+}))
 // 404
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not found', 404));
